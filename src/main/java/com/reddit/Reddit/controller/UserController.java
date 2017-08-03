b4615dbd9e079e29cebb5cdf50ade5ab1f7a3c99
Obitox <bojan0071@hotmail.com>
2017-08-03 21:50:32 +0200

    Imports
    -Redundant imports

--- src/main/java/com/reddit/Reddit/controller/UserController.java
+++ src/main/java/com/reddit/Reddit/controller/UserController.java
@@ -5,7 +5,6 @@ import com.reddit.Reddit.repository.UserRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 import java.util.List;
 
--- src/main/java/com/reddit/Reddit/service/UserService.java
+++ src/main/java/com/reddit/Reddit/service/UserService.java
@@ -1,6 +1,5 @@
 package com.reddit.Reddit.service;
 
-import org.springframework.remoting.jaxws.AbstractJaxWsServiceExporter;
 import org.springframework.stereotype.Service;
 
 @Service
